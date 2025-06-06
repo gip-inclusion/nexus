1cb085629a8bb4a9597af2c0425f3cec930375b8
Jérémy Buget <jbuget@gmail.com>
2025-06-06 16:02:08 +0200

    style: remove DSFR (too much overlap with internal design system)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
     
-    import '../app.css';
-
-  import '@gouvfr/dsfr/dist/dsfr.min.css';
-  import '@gouvfr/dsfr/dist/utility/utility.min.css';
-
-  import { onMount } from 'svelte';
-
-  onMount(async () => {
-    await import('@gouvfr/dsfr/dist/dsfr.module.min.js');
-
-    if (typeof window !== 'undefined' && window.dsfr) {
-      console.log('DSFR JavaScript loaded successfully!');
-    }
-  });
+	import '../app.css';
   
-  let { children } = $props();
+  	let { children } = $props();
 
 </script>
 
@@ -25,9 +12,3 @@
 </svelte:head>
 
 {@render children()}
-
-<style>
-  :global(body) {
-    font-family: "Marianne", Arial, sans-serif;
-  }
-</style>
