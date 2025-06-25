0332feae7dad554a754c15828813f389ab64ab0d
Jérémy Buget <jbuget@gmail.com>
2025-06-25 15:09:32 +0200

    Fix: ServiceRepositoryGrist test assertions
    This commit fixes the test assertions in ServiceRepositoryGrist to
    correctly compare the expected and actual service objects.

--- src/lib/server/service.test.ts
+++ src/lib/server/service.test.ts
@@ -72,49 +72,50 @@ describe('ServiceRepositoryGrist', () => {
 		const actual: Service[] = await repository.listServicesByStructureId(structureId);
 
 		// then
-    const service1 = new Service();
+		const service1 = new Service();
 		service1.id = '1';
 		service1.name = 'Les emplois de l’inclusion';
 		service1.status = 'BROUILLON';
 		service1.perimeter = 'France entière';
 		service1.lastUpdate = new Date('2025-01-27T16:11:22.091825+01:00');
 		service1.synchronized = false;
-		service1.link = 'https://dora.inclusion.beta.gouv.fr/services/plateforme-de-linclu-les-emplois-de-lincl';
+		service1.link =
+			'https://dora.inclusion.beta.gouv.fr/services/plateforme-de-linclu-les-emplois-de-lincl';
 
-    const service2 = new Service(); 
-    service2.id = '2';
+		const service2 = new Service();
+		service2.id = '2';
 		service2.name = 'Immersion Facilitée';
 		service2.status = 'PUBLIÉE';
 		service2.perimeter = 'France entière';
 		service2.lastUpdate = new Date('2025-01-27T16:11:22.091825+01:00');
 		service2.synchronized = false;
-		service2.link = 'https://dora.inclusion.beta.gouv.fr/services/plateforme-de-linclu-immersion-facilitee'; 
-  
-    const expected: Service[] = [service1, service2];
+		service2.link =
+			'https://dora.inclusion.beta.gouv.fr/services/plateforme-de-linclu-immersion-facilitee';
+
+		const expected: Service[] = [service1, service2];
 		expect(actual).toEqual(expected);
 	});
 });
 
 describe('Service', () => {
 	it('isActive should return `true` if status is "PUBLIÉE"', () => {
-	  // given
-    const service = new Service();
-    
-    // when
-    service.status = 'PUBLIÉE';
-    
-    // then
-    expect(service.isActive).toBe(true);
+		// given
+		const service = new Service();
+
+		// when
+		service.status = 'PUBLIÉE';
+
+		// then
+		expect(service.isActive).toBe(true);
 	});
 	it('isActive should return `false` if status is not "PUBLIÉE"', () => {
- // given
-    const service = new Service();
-    
-    // when
-    service.status = 'BROUILLON';
-    
-    // then
-    expect(service.isActive).toBe(false);
-	
+		// given
+		const service = new Service();
+
+		// when
+		service.status = 'BROUILLON';
+
+		// then
+		expect(service.isActive).toBe(false);
 	});
 });
