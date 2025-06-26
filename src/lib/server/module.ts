5b08d4ad9cf99f9f5ceb6b523c8de6495e41eb36
Jérémy Buget <jbuget@gmail.com>
2025-06-26 11:31:40 +0200

    refactor: Refactor ModuleRepository
    Removed `findModulesByStructureId` as it's not implemented.

--- src/lib/server/module.ts
+++ src/lib/server/module.ts
@@ -1,47 +1,38 @@
-import { GristClient, gristClient } from "./grist";
+import { GristClient, gristClient } from './grist';
 
 export class Module {
-  id?: string;
-  app?: string;
-  title?: string;
-  baseline?: string;
-  logo?: string;
-  website?: string;
+	id?: string;
+	app?: string;
+	title?: string;
+	baseline?: string;
+	logo?: string;
+	website?: string;
 }
 
 export interface ModuleRepository {
-  listModules(): Promise<Module[]>;
-  findModulesByStructureId(structureId: string): Promise<Module[]>;
+	listModules(): Promise<Module[]>;
 }
 
 export class ModuleRepositoryGrist implements ModuleRepository {
-  constructor(readonly gristClient: GristClient) { }
+	constructor(readonly gristClient: GristClient) {}
 
-  async listModules(): Promise<Module[]> {
-    const data = await this.gristClient.requestGristTable(
-			'GET',
-			'Modules',
-			`records`
-		);
-    const modules: Module[] =
+	async listModules(): Promise<Module[]> {
+		const data = await this.gristClient.requestGristTable('GET', 'Modules', `records`);
+		const modules: Module[] =
 			data.records?.map((record: { id: number; fields: Record<string, unknown> }) => {
 				const module = new Module();
-  
+
 				module.id = String(record.id);
-        module.app = record.fields['App'] as string;
+				module.app = record.fields['App'] as string;
 				module.title = record.fields['Title'] as string;
 				module.baseline = record.fields['Baseline'] as string;
 				module.logo = record.fields['Logo'] as string;
 				module.website = record.fields['Website'] as string;
-  
+
 				return module;
 			}) || [];
-    return modules;
-  }
-
-  findModulesByStructureId(structureId: string): Promise<Module[]> {
-    throw new Error("Method not implemented.");
-  }  
+		return modules;
+	}
 }
 
 export const moduleRepository = new ModuleRepositoryGrist(gristClient);
